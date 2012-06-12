1f7d96f89c81e081225f3ea1d9176cc10405b5e9
Kostas Agnantis <kagnadis@gmail.com>
2012-06-12 17:51:17 +0300

    Some more test cases
    TODO: find the priority of the conjunction/disjunction operations

--- src/test/java/tests/ConceptFactoryTest.java
+++ src/test/java/tests/ConceptFactoryTest.java
@@ -10,6 +10,7 @@ import uom.dl.elements.BaseConcept;
 import uom.dl.elements.BaseRole;
 import uom.dl.elements.Concept;
 import uom.dl.elements.ConceptFactory;
+import uom.dl.elements.IntersectionConcept;
 import uom.dl.elements.Role;
 
 public class ConceptFactoryTest {
@@ -45,8 +46,8 @@ public class ConceptFactoryTest {
 
 	@Test
 	public void test_simple_not_statement() {
-		Concept concept = factory.not().c(A).union().c(B).build(); //(¬A⊔B)
-		assertEquals(concept.toString(), "(¬A⊔B)");
+		Concept concept = factory.not().c(A).build(); //(¬A)
+		assertEquals(concept.toString(), "¬A");
 	}
 	
 	@Test
@@ -73,7 +74,42 @@ public class ConceptFactoryTest {
 		assertEquals(concept.toString(), "⩽3R.A");
 	}
 	
+	@Test
+	public void test_complex_not_statement() {
+		Concept concept = factory.not().start().c(A).union().c(B).end().build(); //(¬A⊔B)
+		assertEquals(concept.toString(), "¬(A⊔B)");
+	}
 	
+	@Test
+	public void test_complex_atleast_statement() {
+		Concept concept = factory.atleast(3, R).start().c(A).union().c(B).end().build(); //(⩾3R.A)⩾⩽
+		assertEquals(concept.toString(), "⩾3R.(A⊔B)");
+	}
+	
+	@Test
+	public void test_complex_atmost_statement() {
+		Concept concept = factory.atmost(3, R).start().c(A).union().c(B).end().build(); //(⩽3R.A)⩾⩽
+		assertEquals(concept.toString(), "⩽3R.(A⊔B)");
+	}
+	
+	@Test
+	public void test_complex_forall_statement() {
+		Concept concept = factory.forall(R).start().c(A).union().c(B).end().build(); 
+		assertEquals(concept.toString(), "(∀R.(A⊔B))");
+	}
+	
+	@Test
+	public void test_complex_exists_statement() {
+		Concept concept = factory.exists(R).start().c(A).union().c(B).end().build(); 
+		assertEquals(concept.toString(), "(∃R.(A⊔B))");
+	}
+	
+	@Test
+	public void test_complex_intersection_union_statement() {
+		Concept concept = factory.c(A).union().c(B).intersection().c(A).union().c(B).build(); //
+		//assertEquals(concept.toString(), "(((A⊔B)⊓A)⊔B)");
+		assertEquals(concept.toString(), "(A⊔(B⊓A)⊔B)");
+	}
 	
 
 }
