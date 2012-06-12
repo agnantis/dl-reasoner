412f75747a478c5270f12a3d782b68618777c8ba
Kostas Agnantis <kagnadis@gmail.com>
2012-06-12 16:51:08 +0300

    Add some basic JUnit tests for ConceptFactory

---
+++ src/test/java/tests/ConceptFactoryTest.java
@@ -0,0 +1,79 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import uom.dl.elements.BaseConcept;
+import uom.dl.elements.BaseRole;
+import uom.dl.elements.Concept;
+import uom.dl.elements.ConceptFactory;
+import uom.dl.elements.Role;
+
+public class ConceptFactoryTest {
+	Concept A = new BaseConcept("A");
+	Concept B = new BaseConcept("B");
+	Role R = new BaseRole("R");
+	
+	ConceptFactory factory;
+	
+	@Before
+	public void setUp() {
+		factory = new ConceptFactory();
+		A = new BaseConcept("A");
+		B = new BaseConcept("B");
+		R = new BaseRole("R");
+	}
+	
+	@After
+	public void tearDown() {		
+	}
+	
+	@Test
+	public void test_simple_union_statement() {
+		Concept concept = factory.c(A).union().c(B).build(); //(A⊔B)
+		assertEquals(concept.toString(), "(A⊔B)");
+	}
+	
+	@Test
+	public void test_simple_intersection_statement() {
+		Concept concept = factory.c(A).intersection().c(B).build(); //(A⊓B)
+		assertEquals(concept.toString(), "(A⊓B)");
+	}
+
+	@Test
+	public void test_simple_not_statement() {
+		Concept concept = factory.not().c(A).union().c(B).build(); //(¬A⊔B)
+		assertEquals(concept.toString(), "(¬A⊔B)");
+	}
+	
+	@Test
+	public void test_simple_exists_statement() {
+		Concept concept = factory.exists(R).c(A).build(); //(∃R.A)
+		assertEquals(concept.toString(), "(∃R.A)");
+	}
+	
+	@Test
+	public void test_simple_forall_statement() {
+		Concept concept = factory.forall(R).c(A).build(); //(∀R.A)
+		assertEquals(concept.toString(), "(∀R.A)");
+	}
+	
+	@Test
+	public void test_simple_atleast_statement() {
+		Concept concept = factory.atleast(3, R).c(A).build(); //(⩾3R.A)⩾⩽
+		assertEquals(concept.toString(), "⩾3R.A");
+	}
+	
+	@Test
+	public void test_simple_atmost_statement() {
+		Concept concept = factory.atmost(3, R).c(A).build(); //(⩽3R.A)
+		assertEquals(concept.toString(), "⩽3R.A");
+	}
+	
+	
+	
+
+}
