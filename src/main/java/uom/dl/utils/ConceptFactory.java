2b2c5389629e716617c644c5b5970458d3059d5a
Kostas Agnantis <kagnadis@gmail.com>
2012-06-13 20:55:42 +0300

    User Set<Concept> instead of List<Concept>

--- src/main/java/uom/dl/utils/ConceptFactory.java
+++ src/main/java/uom/dl/utils/ConceptFactory.java
@@ -1,61 +1,101 @@
 package uom.dl.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
+import uom.dl.elements.AtomicConcept;
 import uom.dl.elements.Concept;
+import uom.dl.elements.ConceptBuilder;
 import uom.dl.elements.Constants;
 import uom.dl.elements.IntersectionConcept;
 import uom.dl.elements.UnionConcept;
 
 public class ConceptFactory {
 	
-	public static Concept unionOfConcepts(List<Concept> concepts) {
+	public static Concept unionOfConcepts(Set<Concept> concepts) {
 		if (concepts == null || concepts.isEmpty())
 			return Constants.TOP_CONCEPT;
 		
-		Concept whole = concepts.get(concepts.size()-1);
+		Iterator<Concept> it = concepts.iterator();
+		Concept whole = it.next();
 		
-		for (int i = concepts.size() - 2; i >= 0 ; --i){
-			whole = new UnionConcept(concepts.get(i), whole);
-		}
+		while (it.hasNext())
+			whole = new UnionConcept(it.next(), whole);
+		
 		return whole;
 	}
 	
-	public static Concept intersectionOfConcepts(List<Concept> concepts) {
+	public static Concept intersectionOfConcepts(Set<Concept> concepts) {
 		if (concepts == null || concepts.isEmpty())
 			return Constants.TOP_CONCEPT;
 		
-		Concept whole = concepts.get(concepts.size()-1);
+		Iterator<Concept> it = concepts.iterator();
+		Concept whole = it.next();
 		
-		for (int i = concepts.size() - 2; i >= 0 ; --i){
-			whole = new IntersectionConcept(concepts.get(i), whole);
-		}
+		while (it.hasNext())
+			whole = new IntersectionConcept(it.next(), whole);
+
 		return whole;
 	}
 	
-	public static List<Concept> getIntersectionConcepts(IntersectionConcept concept) {
-		List<Concept> conceptList = new ArrayList<>();
+	public static Set<Concept> getIntersectionConcepts(Concept concept) {
+		if (!(concept instanceof IntersectionConcept)) {
+			Set<Concept> set = new HashSet<>(1);
+			set.add(concept);
+			return set;
+		}
+
+		Set<Concept> conceptList = new HashSet<>();
 		Concept conceptA = concept.getConceptA();
-		if (conceptA instanceof IntersectionConcept)
-			conceptList.addAll(getIntersectionConcepts((IntersectionConcept) conceptA));
-		Concept conceptB = concept.getConceptB();
-		if (conceptB instanceof IntersectionConcept)
-			conceptList.addAll(getIntersectionConcepts((IntersectionConcept) conceptB));
+		conceptList.addAll(getIntersectionConcepts(conceptA));
+		
+		Concept conceptB = ((IntersectionConcept)concept).getConceptB();
+		conceptList.addAll(getIntersectionConcepts(conceptB));
 		
 		return conceptList;
 	}
 	
-	public static List<Concept> getUnionConcepts(UnionConcept concept) {
-		List<Concept> conceptList = new ArrayList<>();
+	public static Set<Concept> getUnionConcepts(Concept concept) {
+		if (!(concept instanceof UnionConcept)) {
+			Set<Concept> set = new HashSet<>(1);
+			set.add(concept);
+			return set;
+		}
+		
+		Set<Concept> conceptList = new HashSet<>();
 		Concept conceptA = concept.getConceptA();
-		if (conceptA instanceof UnionConcept)
-			conceptList.addAll(getUnionConcepts((UnionConcept) conceptA));
-		Concept conceptB = concept.getConceptB();
-		if (conceptB instanceof IntersectionConcept)
-			conceptList.addAll(getUnionConcepts((UnionConcept) conceptB));
+		conceptList.addAll(getUnionConcepts(conceptA));
+		Concept conceptB = ((UnionConcept)concept).getConceptB();
+		conceptList.addAll(getUnionConcepts(conceptB));
 		
 		return conceptList;
+	}
+	
+	public static void main(String[] main) {
+		ConceptBuilder factory = new ConceptBuilder();
+		AtomicConcept A = new AtomicConcept("A");
+		AtomicConcept B = new AtomicConcept("B");
+		Concept concept;//= factory.c(A).union().c(B).intersection().c(A).union().c(B).build();
+		Set<Concept> conSet = new HashSet<>(Arrays.asList(A, B, new IntersectionConcept(A, B)));
+		for (Concept c : conSet) {
+			System.out.println(">> " + c);
+		}
+		concept = unionOfConcepts(conSet);
+		Set<Concept> unionConcepts = getUnionConcepts(concept);
+		System.out.println(concept.toString());
+		for (Concept c : unionConcepts)
+			System.out.println("\t" + c);
+		
+		conSet = new HashSet<>(Arrays.asList(A, B, new IntersectionConcept(A, B)));
+		concept = intersectionOfConcepts(conSet);
+		Set<Concept> intersectionConcepts = getIntersectionConcepts(concept);
+		System.out.println("\n" +concept.toString());
+		for (Concept c : intersectionConcepts)
+			System.out.println("\t" + c);
 	}
 
 }
